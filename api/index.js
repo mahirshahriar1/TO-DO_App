5142539ca62d55c0ec4940c8115a5909b9c28953
unknown <03mahirshahriar@gmail.com>
2024-02-25 23:54:05 +0600

    first commit

--- api/index.js
+++ api/index.js
@@ -2,17 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const crypto = require("crypto");
+const dotenv = require("dotenv")
+dotenv.config()
 
 const app = express();
 const port = 3000;
 const cors = require("cors");
+
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongoose
-  .connect("mongodb+srv://mahir:mahir@cluster0.vtfsvyt.mongodb.net/")
+  .connect(process.env.MONGODB_CONNECT_URL)
   .then(() => {
     console.log("Connected to database!");
   })
